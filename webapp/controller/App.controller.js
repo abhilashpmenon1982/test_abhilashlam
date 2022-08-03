69438613f773f4c422c88b623372b9ea2710f433
Menon, Abhilash <Abhilash.Menon@lamresearch.com>
2022-08-03 12:25:02 +0530

    first version

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+
+    'lam/fin/ar/controller/BaseController'
+
+], function(BaseController) {
+
+    'use strict';
+
+    return BaseController.extend("lam.fin.ar.controller.App",{
+
+        onInit: function(){
+
+           
+
+        }
+
+    });
+
+});
